e9d76a86ff85c8dc3379df8483a2e391941d576c
Nitin Tiwari <97907417+nitinkt09@users.noreply.github.com>
2024-08-08 01:44:03 +0530

    Add files via upload
    Admin page (Admin who controls the whole mini gird )

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Total Connections </h1>
+    <br>
+    <table>
+        <thead>
+
+            <tr>
+                <th>DERMS ID</th>
+                <th>Username</th>
+                <th>Date</th>
+                <th>Email</th>
+                <th>Phone number </th>
+                <th>Address</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>001</td>
+                <td>Nitn@</td>
+                <td>sfffffffffs</td>
+                <td>tiwarinitn3009@gmail.com</td>
+                <td>7898951340</td>
+                <td>india</td>
+                <td>
+                    <a href="update">update</a>
+                    <a href="delete">delete</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
